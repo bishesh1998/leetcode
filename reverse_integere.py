a3fe11f902f6aa5e64c1dd4fe364b48c338feb27
Bishesh Tuladhar <50055489+bishesh1998@users.noreply.github.com>
2022-02-25 12:16:45 -0800

    Create reverse_integere.py

---
+++ reverse_integere.py
@@ -0,0 +1,11 @@
+def reverse(self, x: int) -> int:
+  y = str(abs(x))
+  y = y.strip()
+  y = y[::-1]
+  output = int(y)
+  if output >= 2** 31 -1 or output <= -2** 31:
+      return 0
+  elif x < 0:
+      return -1 * output
+  else:
+      return output
